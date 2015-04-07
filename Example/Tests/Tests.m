4d94939e4aacaf8b0d5882448129e087702b59e0
@hunk <ing.edgar@gmail.com>
2015-04-07 12:54:21 -0500

    add xctest

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -6,4 +6,24 @@
 //  Copyright (c) 2014 @hunk. All rights reserved.
 //
 
-${TEST_EXAMPLE}
+#import <XCTest/XCTest.h>
+
+@interface Tests : XCTestCase
+
+@end
+
+@implementation Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+@end
